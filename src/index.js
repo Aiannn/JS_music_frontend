51f9a064edc21ffcd5e6a137339d70678a42dc0b
Aiannn <soktoevayan@gmail.com>
2020-08-17 17:25:01 -0400

    second draft

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //#1 
+//let num = Math.floor(Math.random()*100)
 for (let num=1; num<6; num++)
+//for (num; num<num+5; num++)
 fetch(`http://localhost:3000/artists/${num}`)
 .then(response => response.json())
 .then(artist => {
@@ -9,19 +11,19 @@ fetch(`http://localhost:3000/artists/${num}`)
 
 //#2
 function getTracks(num) {
-    fetch(`https://api.deezer.com/artist/${num}/top?limit=5`)
+    fetch(`http://localhost:3000/songs/${num}`) 
     .then(response => response.json())
     .then(tracklist => {
-        // console.log(tracklist)
-        tracklist.forEach(track => {//it doesn't work!!
-            const artistItem = document.querySelector(`div[data-id=${num}]`)
+        tracklist.data.forEach(track => {
+            const artistItem = document.querySelector(`div[data-id="${num}"]`)
             const trackList = document.createElement('ul')
             trackList.innerHTML = `
                 <li>${track.title}</li>
                 <li>${track.duration}</li>
-                <li>${track.album}</li>
+                <li>${track.album.title}</li>
                 <li>${track.release_date}</li>
-                <li>${track.preview}</li>
+                <li><audio id="audio" src=${track.preview} controls></li>
+                <button id="fav">To Favorite</button>
             `
         artistItem.append(trackList) 
         })
@@ -37,8 +39,7 @@ function renderArtist(artist) {
     const artistItem = document.createElement('div')
     artistItem.innerHTML = `
         <p>${artist.name}</p>
-        <img src = ${artist.picture_medium} >
-        <p>top songs: ${artist.tracklist}</p>
+        <img src = ${artist.picture_medium}>
         <button class='info'>INFO</button>
     `
     artistItem.dataset.id = artist.id
